362d4223c8ac824e3e77666f4f6a3ac92a60dd82
RobertRobii <robertraiovici01@gmail.com>
2023-12-14 22:09:31 +0200

    updated footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <p className="orange_gradient font-bold mt-10 mb-10 text-xl">
+    <p className="orange_gradient font-bold mt-10 mb-10 text-xl text-center">
       © Robert Raiovici - JavaScript Explorer 2023
     </p>
   );
